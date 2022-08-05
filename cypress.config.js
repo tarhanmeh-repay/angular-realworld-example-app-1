05b20a2be98c2e05e5ce494bbf7b48eb7ae475c5
tarhanmeh-repay <mtarhan@repay.com>
2022-08-05 12:08:50 -0500

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,41 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+
+  "retries": {
+    "runMode": 2,
+    "openMode": 1,
+  },
+
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
+  reporterEnabled: 'spec, mocha-junit-reporter',
+  mochaJunitReporterReporterOptions: {
+    mochaFile: 'cypress/results/results-[hash].xml'
+  },
+
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
+  e2e: {
+    baseUrl: "http://localhost:4200/",
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
+  },
+  "env": {
+    "username": "yedekmehmettarhan@gmail.com",
+    "password": "srntrhn.2134",
+    "apiUrl": "https://api.realworld.io/"
+  }
+})
+
